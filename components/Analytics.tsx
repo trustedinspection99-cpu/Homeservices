d5a96014e88c42d1cf6a84d09bf4e0d412e6e6e4
trustedinspection99-cpu <trustedinspection99@gmail.com>
2026-01-29 18:12:32 -0500

    Update Analytics.tsx

---
+++ components/Analytics.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { trackPageView } from '@/lib/analytics';
+
+export function Analytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      trackPageView(pathname);
+    }
+  }, [pathname, searchParams]);
+
+  if (!process.env.NEXT_PUBLIC_GA_ID) {
+    return null;
+  }
+
+  return (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
